dfeea2c0b83fdfedb2089fc4876971b620217a77
Roman Pirag <romanpirag@gmail.com>
2019-07-26 16:29:30 -0400

    palindrome

--- algorithms.js
+++ algorithms.js
@@ -1,5 +1,21 @@
 // Welcome to algorithms🦄
 
+// Write a method that takes a string and returns the number of vowels
+// in the string. You may assume that all the letters are lower cased.
+// You can treat "y" as a consonant.
 
+function palindrome(string) {
+  
+    for (let i = 0; i < string.length; i++) {
+        for (let j = string.length - 1; j >= 0; j--) {
+            if (string[j] !== string[i]) {
+                return false 
+            }
+        }
+    }
+  
+    return true 
 
+}
 
+console.log(palindrome("racecar"))
