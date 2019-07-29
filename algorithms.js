fd5f13f903a7e8db34e6c63ee11896c8e3de319d
Roman Pirag <romanpirag@gmail.com>
2019-07-29 12:42:21 -0400

    Palindrome

--- algorithms.js
+++ algorithms.js
@@ -4,18 +4,24 @@
 // in the string. You may assume that all the letters are lower cased.
 // You can treat "y" as a consonant.
 
-function palindrome(string) {
-  
-    for (let i = 0; i < string.length; i++) {
-        for (let j = string.length - 1; j >= 0; j--) {
-            if (string[j] !== string[i]) {
-                return false 
-            }
-        }
-    }
-  
-    return true 
+// 1. Going to create create a palindrome function
+// 2. Going to split the string that is given which creates an array of letters
+// 3. I will iterate through the string with two for loops, one going forwards and one backwards
+// 4. I will store the respective results in new variables, one titled forward, the other named back
+// 5. I will compare the forward and back variables, and return false if they are ever different from eachother.
+// 6. The function will return true if the false condition is never met.
 
+const palindrome = string => {
+    let reversed = []
+    for(let i = 0; i < string.length; i++) {
+        reversed = string[i] + reversed 
+    }
+    
+    if(string !== reversed) {
+        return "NOT PALINDROME"
+    }
+
+    return "PALINDROME!!!"
 }
 
 console.log(palindrome("racecar"))
