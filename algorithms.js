d36b9b701e5dc69a737308b7b70d64c49c4bc180
Roman Pirag <romanpirag@gmail.com>
2019-07-29 13:00:30 -0400

    NearbyAZ

--- algorithms.js
+++ algorithms.js
@@ -1,27 +1,22 @@
 // Welcome to algorithms🦄
 
-// Write a method that takes a string and returns the number of vowels
-// in the string. You may assume that all the letters are lower cased.
-// You can treat "y" as a consonant.
+// Write a method that takes a string in and returns true if the letter
+// "z" appears within three letters **after** an "a". You may assume
+// that the string contains only lowercase letters.
 
-// 1. Going to create create a palindrome function
-// 2. Going to split the string that is given which creates an array of letters
-// 3. I will iterate through the string with two for loops, one going forwards and one backwards
-// 4. I will store the respective results in new variables, one titled forward, the other named back
-// 5. I will compare the forward and back variables, and return false if they are ever different from eachother.
-// 6. The function will return true if the false condition is never met.
-
-const palindrome = string => {
-    let reversed = []
-    for(let i = 0; i < string.length; i++) {
-        reversed = string[i] + reversed 
+const nearbyAZ = string => {
+  for (let i = 0; i < string.length; i++) {
+    let g = i + 3
+    if (string[i] === "a") {
+      for (let j = i + 1; j <= g; j++) {
+        if (string[j] === "z") {
+          return true
+        }
+      }
     }
-    
-    if(string !== reversed) {
-        return "NOT PALINDROME"
-    }
+  }
 
-    return "PALINDROME!!!"
+  return false
 }
 
-console.log(palindrome("racecar"))
+console.log(nearbyAZ("bbbbbabbzbbbb"))
