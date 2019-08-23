95745e9a0c167e31959e763bff5991f42ca8f08a
Roman Pirag <romanpirag@gmail.com>
2019-08-23 17:09:03 -0400

    Completed Reverse String Algo with Steps

--- algorithms.js
+++ algorithms.js
@@ -6,7 +6,7 @@
 // 3. I am going to declaire a variable called stringSlice and set it equal to the string argument and use the builtin method .slice with two ("") inside
 // 4. This creates a new array with every element of the string divided 
 // 7. I am going to declair a new variable called reversed and set it equal to an empty array 
-// 5. Now I am going to use the built in method .map on the stringSlice array
+// 5. Now I am going to use the built in method .map on the stringSplit array
 // 6. The map() method creates a new array with the results of calling a provided function on every element in the calling array.
 // 8. Within the .map method I am going to .unshift every element of the array into the newly made reversed array.
 // 9. I am going to return the reversed array after I use the .join method in order to make it back into a string
@@ -14,11 +14,12 @@
 
 
 const reverseString = (string) => {
-  let stringSlice = string.slice("")
-  stringSlice.map(letter => {
-    
+  let stringSplit = string.split("")
+  let reversed = []
+  stringSplit.map(letter => {
+    reversed.unshift(letter)
   })
-
+  return reversed.join("")
 }
 
 
