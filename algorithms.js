a5bc9bf00493d3f50ca4684ba71292c3c455db43
Roman Pirag <romanpirag@gmail.com>
2019-08-23 17:03:12 -0400

    Wrote the steps for reverse string

--- algorithms.js
+++ algorithms.js
@@ -1,6 +1,25 @@
 // Welcome to algorithms🦄
 
 
+// 1. I am going to Create a function called reverse String
+// 2. I am going to pass in the sting as an argument 
+// 3. I am going to declaire a variable called stringSlice and set it equal to the string argument and use the builtin method .slice with two ("") inside
+// 4. This creates a new array with every element of the string divided 
+// 7. I am going to declair a new variable called reversed and set it equal to an empty array 
+// 5. Now I am going to use the built in method .map on the stringSlice array
+// 6. The map() method creates a new array with the results of calling a provided function on every element in the calling array.
+// 8. Within the .map method I am going to .unshift every element of the array into the newly made reversed array.
+// 9. I am going to return the reversed array after I use the .join method in order to make it back into a string
+
+
+
+const reverseString = (string) => {
+  let stringSlice = string.slice("")
+  stringSlice.map(letter => {
+    
+  })
+
+}
 
 
 
@@ -9,20 +28,5 @@
 
 
 
+console.log(reverseString("LOKI"))
 
-
-
-
-
-// if (num >59){
-  //   let hours = Math.floor(num / 60)
-  //   let minutes = num%60
-  //   if (minutes <10){
-  //     return `${hours}:0${minutes}`
-  //   }
-  //   return `${hours}:${minutes}`
-  // } if (num < 10){
-  //   return  `0:0${num}`
-  // }
-  // return `0:${num}`
-  
